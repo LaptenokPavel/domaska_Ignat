46be15d8d06b90d58d2b9234102712f8cc32b29c
Laptenok Pavel <pavlap82@mail.ru>
2021-07-13 20:07:42 +0300

    13.07

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,31 +1,9 @@
 import React from 'react'
-import s from './Message.module.css'
 
-type MessagePropsTape = {
-    avatar: string
-    name: string
-    message: string
-    time: any
-}
-
-function Message(props: MessagePropsTape) {
+function Message() {
     return (
         <div>
-            <div className={s.wrapper}>
-                <div className={s.img}>
-                    <img src={props.avatar} alt=""/>
-                </div>
 
-                <div className={s.messages}>
-                    <div className={s.text}>
-                        <div className={s.name}>{props.name}
-                        </div>
-                        <div className={s.message}>{props.message}
-                        </div>
-                    </div>
-                    <div className={s.time}>{props.time}</div>
-                </div>
-            </div>
         </div>
     )
 }
