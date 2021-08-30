e387e7cbd3ff325f3800e1185ed05f157b0451af
LaptenokPavel <1982pavlap@gmail.com>
2021-08-30 20:47:51 +0300

    30.08-1

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <NavLink to = '/pre-junior'>PreJunior</NavLink>
+            <NavLink to = '/error404'>Error404</NavLink>
         </div>
     )
 }
