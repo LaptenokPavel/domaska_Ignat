5e2c5d91f1d931db4acc0b79cc42fdead0e5b5d9
LaptenokPavel <1982pavlap@gmail.com>
2021-10-12 19:31:19 +0300

    12.09(Hometask №9)

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -2,30 +2,71 @@ import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
+
+
+
+
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
+    let hours = date.getHours()
+    let minits = date.getMinutes()
+    let seconds = date.getSeconds()
+    let day = date.getDate()
+    let month = date.getMonth()
+    let year = date.getFullYear()
+
+    const multiplay = (value:number) => value < 10 ? '0' + value : value
+
+    const clockStyle = {
+        width: '80px',
+        border: '1px solid green',
+        backgroundColor: 'aquamarine',
+        marginTop: '20px'
+    }
+
+    const dateStyle = {
+        width: '80px',
+        border: '1px solid red',
+        backgroundColor: 'orange',
+        marginTop: '20px',
+        marginBottom: '20px'
+    }
+
+    const buttonBackground = {backgroundColor: 'green'}
+
     const stop = () => {
-        // stop
+        clearTimeout(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+             setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime =  <div style={clockStyle}>
+        <span>{multiplay(hours)}:</span>
+        <span>{multiplay(minits)}:</span>
+        <span>{multiplay(seconds)}</span>
+    </div> // fix with date
+
+
+    const stringDate = <div style={dateStyle}>
+        <span>{multiplay(day)}:</span>
+        <span>{multiplay(month)}:</span>
+        <span>{multiplay(year)}</span>
+    </div>
+    // fix with date
 
     return (
         <div>
@@ -34,6 +75,7 @@ function Clock() {
                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
+
             </div>
 
             {show && (
@@ -42,8 +84,8 @@ function Clock() {
                 </div>
             )}
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <SuperButton style={buttonBackground} onClick={start}>start</SuperButton>
+            <SuperButton style={buttonBackground} onClick={stop}>stop</SuperButton>
 
         </div>
     )
